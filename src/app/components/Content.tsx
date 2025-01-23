40f38543558c65ed157ba8eb21d88aec78de83e3
kruizo <kynethruizo@gmail.com>
2025-01-24 06:26:30 +0800

    added tv section

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -6,12 +6,16 @@ import RecommendedListCards from "@components/RecommendedListCards";
 import TrendingList from "@components/TrendingList";
 import Sidepanel from "@components/Sidepanel";
 import UpcomingList from "@components/UpcomingList";
+import PopularTVListCards from "./PopularTVListCards";
 
 const Content = () => {
   return (
     <Fragment>
       <div className="max-w-6xl px-5 flex gap-6">
-        <RecommendedListCards />
+        <div className="space-y-2">
+          <RecommendedListCards />
+          <PopularTVListCards />
+        </div>
         <div className="pt-20">
           <Sidepanel>
             <TrendingList />
---
+++ src/app/components/PopularTVListCards.tsx
@@ -0,0 +1,116 @@
+import { IMovieCollection } from "@customTypes/index";
+import CardsList from "./CardsList";
+import { fetchPopularTV } from "../api";
+import { Fragment, useState, useEffect } from "react";
+
+const PopularTVListCards = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recommendedMovies, setRecommendedMovies] =
+    useState<IMovieCollection | null>(null);
+  const [hoverMovies, setHoverMovies] = useState<{
+    [key: number]: IMovieCollection;
+  }>({});
+  const [isFetching, setIsFetching] = useState(false);
+
+  const prefetchMovies = async (page: number) => {
+    if (hoverMovies[page] || isFetching) return;
+    try {
+      console.log(`Pre-fetching movies for page ${page}...`);
+      const response = await fetchPopularTV(page);
+      setHoverMovies((prev) => ({ ...prev, [page]: response }));
+      console.log(`Pre-fetched movies for page ${page}:`, response);
+    } catch (error) {
+      console.error("Error prefetching movies:", error);
+    }
+  };
+
+  const handlePageChange = async (page: number) => {
+    if (isFetching || page < 1 || page > (recommendedMovies?.total_pages || 1))
+      return;
+
+    setIsFetching(true);
+    try {
+      console.log(`Fetching movies for page ${page}...`);
+      const response = hoverMovies[page] || (await fetchPopularTV(page));
+      setRecommendedMovies(response);
+      setCurrentPage(page);
+      console.log(`Movies updated for page ${page}`);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        console.log("Fetching movies...");
+
+        const response = await fetchPopularTV();
+        setRecommendedMovies(response);
+        console.log(response);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+    getMovies();
+  }, []);
+  return (
+    <Fragment>
+      {recommendedMovies && (
+        <div className="py-7">
+          <div className="flex justify-between ">
+            <div className="flex gap-2 items-center">
+              <h1 className="border-l-[var(--accent)] border-l-[12px] px-4 text-3xl font-extrabold text-primary-content">
+                RECOMMENDED
+              </h1>
+              <button className="text-sm rounded-lg px-2 py-1 border border-[var(--accent)] text-[var(--accent)] hover:text-white hover:bg-[var(--accent)] hover:border-[var(--accent)] transition-colors">
+                MOVIES
+              </button>
+              <button className="text-sm rounded-lg px-2 py-1 border border-[var(--base-gray)] text-[var(--base-gray)] hover:text-white hover:bg-[var(--accent)] hover:border-[var(--accent)] transition-colors">
+                TV SHOWS
+              </button>
+            </div>
+            <div className="flex justify-center items-center gap-2 ">
+              <button
+                className={` px-3 py-2 transition-colors rounded-md ${
+                  currentPage != 1
+                    ? "text-[var(--accent)] hover:text-[var(--text-white)] hover:bg-[var(--accent)] border border-[var(--accent)]"
+                    : "border border-[var(--base-gray)] text-[var(--base-gray)] "
+                }`}
+                disabled={currentPage === 1}
+                onClick={() => handlePageChange(currentPage - 1)}
+              >
+                <i
+                  className="fa fa-angle-left"
+                  style={{ fontSize: "25px" }}
+                ></i>
+              </button>
+              <button
+                className={` px-3 py-2 transition-colors rounded-md ${
+                  currentPage != recommendedMovies.total_pages
+                    ? "text-[var(--accent)] hover:text-[var(--text-white)] hover:bg-[var(--accent)] border border-[var(--accent)]"
+                    : "border border-[var(--base-gray)] text-[var(--base-gray)] "
+                }`}
+                disabled={currentPage === recommendedMovies.total_pages}
+                onClick={() => handlePageChange(currentPage + 1)}
+                onMouseEnter={() => prefetchMovies(currentPage + 1)}
+              >
+                <i
+                  className="fa fa-angle-right"
+                  style={{ fontSize: "25px" }}
+                ></i>
+              </button>
+            </div>
+          </div>
+          <div className="flex py-4">
+            <CardsList movies={recommendedMovies} />
+          </div>
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+export default PopularTVListCards;
