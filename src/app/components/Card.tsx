a16a42c4b1b77afee8f0826adfe3920f59969782
kruizo <kynethruizo@gmail.com>
2025-01-24 05:27:51 +0800

    updated card ui

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,29 +1,22 @@
 import React from "react";
-import { IMovieCollection } from "@customTypes/index";
+import { normalizeRating } from "@utils/script";
 
 interface CardProps {
   title: string;
   poster_path: string;
-  overview: string;
   release_date: string;
   vote_average: number;
-  vote_count: number;
-  genre_ids: number[];
-  popularity: number;
-  original_language: string;
+  media_type: string;
 }
 
 const Card: React.FC<CardProps> = ({
   title,
   poster_path,
-  overview,
   release_date,
   vote_average,
-  vote_count,
-  genre_ids,
-  popularity,
-  original_language,
+  media_type,
 }) => {
+  const normalizedRating = normalizeRating(vote_average);
   return (
     <div className="bg-base-100 shadow-xl rounded-md w-full">
       <div className="h-72">
@@ -33,7 +26,20 @@ const Card: React.FC<CardProps> = ({
           className="h-full w-full object-cover rounded-md object-center"
         />
       </div>
-      <div className="card-body py-3 px-0 h-14">
+      <div className="card-body py-3 px-0 h-[6rem]">
+        <div className="flex justify-between items-center py-1">
+          <span className="text-sm text-[var(--base-gray)]">
+            {release_date.split("-")[0]}
+          </span>
+          <span className="text-sm text-[var(--base-gray)]">
+            {media_type.substring(0, 1).toUpperCase() + media_type.substring(1)}
+          </span>
+          <span className="text-sm text-[var(--base-gray)] flex gap-1 items-center">
+            {/* <span className="scale-150  -mt-[1px]">☆</span> */}
+            <i className="fa fa-star text-[var(--base-gray)]"></i>
+            {normalizedRating}
+          </span>
+        </div>
         <h2 className="card-title text-sm font-normal line-clamp-2">{title}</h2>
       </div>
     </div>
